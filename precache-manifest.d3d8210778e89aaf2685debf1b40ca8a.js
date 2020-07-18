3bf37f70a38a1fbfb69499affcd2627503c610a9
ignacio <ignaciorobledounq@gmail.com>
2020-07-18 01:44:11 -0300

    Updates

---
+++ precache-manifest.d3d8210778e89aaf2685debf1b40ca8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3320c0caff56509b5e91c56aefe208cf",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "c6dabe22066a4d3e0d5a",
+    "url": "/game-of-life/static/css/main.1da06f91.chunk.css"
+  },
+  {
+    "revision": "3471929c53e0ade34dae",
+    "url": "/game-of-life/static/js/2.42529aa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.42529aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6dabe22066a4d3e0d5a",
+    "url": "/game-of-life/static/js/main.3f017a2f.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
